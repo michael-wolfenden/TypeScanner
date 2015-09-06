c0a773f1f9e9a93454b6382d18829db166930e4c
Michael Wolfenden <michael.wolfenden@gmail.com>
2015-09-06 19:58:30 +1000

    reset GlobalAssemblyInfo

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -2,9 +2,9 @@ using System.Reflection;
 
 [assembly: AssemblyDescription("Fluent interface for scanning for types")]
 [assembly: AssemblyProduct("TypeScanner")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0+2.Branch.master.Sha.73d8674464f3ce3aa4b1e6dff757a4753c9dd11c")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
 [assembly: AssemblyCopyright("Copyright © Michael Wolfenden 2015")]
 
 #if DEBUG
